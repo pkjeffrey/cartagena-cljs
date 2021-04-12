b8cce3962aa1c2430e61cb5aa013823b470821e7
P.Jeffrey <peterk.jeffrey@sa.gov.au>
2021-04-12 21:39:49 +0930

    new-game

--- src/cljs/cartagena/app.cljs
+++ src/cljs/cartagena/app.cljs
@@ -1,24 +1,42 @@
 (ns cartagena.app
-  (:require [cartagena.core :as c]
-            [cartagena.events]
+  (:require [cartagena.events]
+            [clojure.string :as str]
+            [reagent.core :as r]
             [reagent.dom :as rdom]
             [re-frame.core :as rf]))
 
-(defn inp []
-  (let [name @(rf/subscribe [:name])]
-    [:div
-     "Name: " [:input {:type "text"
-                       :value name
-                       :on-change #(rf/dispatch [:name (-> % .-target .-value)])}]]))
+(defn player-input [a ph]
+  [:input {:type "text"
+           :placeholder ph
+           :value @a
+           :on-change #(reset! a (-> % .-target .-value))}])
 
-(defn out []
-  (let [name @(rf/subscribe [:name])]
-    [:p "Greeting: Hello " name]))
+(defn new-game []
+  (let [player1 (r/atom nil)
+        player2 (r/atom nil)
+        player3 (r/atom nil)
+        player4 (r/atom nil)
+        player5 (r/atom nil)]
+    (fn []
+      [:div.player-input
+       [:div
+        [:h1 "Cartagena-cljs"]
+        [:p "Enter player names:"]
+        [player-input player1 "Player one (required)"]
+        [player-input player2 "Player two (required)"]
+        [player-input player3 "Player three"]
+        [player-input player4 "Player four"]
+        [player-input player5 "Player five"]
+        [:input.btn {:type     "button"
+                     :value    "Start Game"
+                     :disabled (or (str/blank? @player1)
+                                   (str/blank? @player2))
+                     :on-click #(rf/dispatch [:new-game (->> [@player1 @player2 @player3 @player4 @player5]
+                                                             (filter (comp not str/blank?)))])}]]])))
 
 (defn page []
   [:div
-   [inp]
-   [out]])
+   [new-game]])
 
 (rdom/render [#'page]
              (.getElementById js/document "app"))
--- src/cljs/cartagena/events.cljs
+++ src/cljs/cartagena/events.cljs
@@ -1,6 +1,15 @@
 (ns cartagena.events
   (:require
+   [cartagena.core :as c]
    [re-frame.core :as rf]))
+
+(rf/reg-event-db
+ :new-game
+ (fn [db [_ players]]
+   (-> db
+       (assoc :game-state :playing
+              :winner nil
+              :game (c/init-game players)))))
 
 (rf/reg-event-db
  :name
